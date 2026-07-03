08bc6227b8c086f23db52c67e33774d0ce348480
OsamaElnahas <osamaelnahas43@gmail.com>
2026-07-03 18:11:48 +0300

    adding auth manager, auth dto

---
+++ SafePharma.BLL/DTOs/AuthenticationDTOs/TokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SafePharma.BLL.DTOs
+{
+    public record TokenDto(
+     string AccessToken,
+     int DurationInMinutes,
+     string TokenType = "Bearer"
+ );
+
+}
---
+++ SafePharma.BLL/Managers/AuthenticationManager/IAuthManager.cs
@@ -0,0 +1,10 @@
+﻿using SafePharma.BLL.DTOs;
+using SafePharma.Common;
+
+namespace SafePharma.BLL.Managers
+{
+    public interface IAuthManager
+    {
+        Task<GeneralResult<TokenDto>> LoginAsync(LoginDTO dto);
+    }
+}
