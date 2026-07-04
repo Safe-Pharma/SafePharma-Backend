419ef978cef9b5ca8eebefc45b84e3632819538d
abassy29 <mostafa.mamdouh1002@gmail.com>
2026-07-04 04:13:12 +0300

    Revert "Revert "crud(user)""

--- SafePharma.BLL/ServicesExtention/BLLServicesExtention.cs
+++ SafePharma.BLL/ServicesExtention/BLLServicesExtention.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using SafePharma.BLL.Managers.users;
+using SafePharma.Common;
 using SafePharma.DAL;
 
 namespace SafePharma.BLL
@@ -16,6 +18,9 @@ namespace SafePharma.BLL
             services.AddScoped<ISubscriptionManager, SubscriptionManager>();
             services.AddScoped<IPasswordHasher<PrimaryContact>, PasswordHasher<PrimaryContact>>();
             services.AddScoped<ITaxManager, TaxManager>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICurrentUserContext, HttpCurrentUserContext>();
+
             services.AddScoped<IUserLanguageManager, UserLanguageManager>();
         }
     }
---
+++ SafePharma.DAL/Migrations/20260703232913_user-tenant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafePharma.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class usertenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PharmacyId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PharmacyId",
+                table: "AspNetUsers",
+                column: "PharmacyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Pharmacies_PharmacyId",
+                table: "AspNetUsers",
+                column: "PharmacyId",
+                principalTable: "Pharmacies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Pharmacies_PharmacyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PharmacyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PharmacyId",
+                table: "AspNetUsers");
+        }
+    }
+}
