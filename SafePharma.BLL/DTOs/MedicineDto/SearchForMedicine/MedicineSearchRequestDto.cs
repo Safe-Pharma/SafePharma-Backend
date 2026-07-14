d26a086f337cac247231548e0d2bf424bb6cbe8e
Ziad Zaki <ziad.mahmoud151@gmail.com>
2026-07-14 18:47:23 +0300

    Add feature search for medicine

---
+++ SafePharma.BLL/DTOs/MedicineDto/SearchForMedicine/MedicineSearchRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SafePharma.BLL
+{
+    public class MedicineSearchRequestDto
+    {
+        public string Query { get; set; } = string.Empty;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
