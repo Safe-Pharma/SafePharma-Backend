df715336245eeb43e26b53e5eee6ed65fd5fc537
Mostafa <Mostafa.mamdouh1002@gmail.com>
2026-07-16 18:38:17 +0300

    convert list<> to Hashset<>

--- SafePharma.DAL/Data/Models/Customer.cs
+++ SafePharma.DAL/Data/Models/Customer.cs
@@ -24,16 +24,14 @@ using SafePharma.DAL;
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
 
+
+
+
+    public ICollection<CustomerAllergy> CustomerAllergies { get; set; } = new HashSet<CustomerAllergy>();
+
+    public ICollection<CustomerChronicCondition> CustomerChronicConditions { get; set; } = new HashSet<CustomerChronicCondition>();
+
     public virtual ICollection<CustomerMedicineHistory> MedicineHistory { get; set; } = new HashSet<CustomerMedicineHistory>();
-
-
-
-    public ICollection<CustomerAllergy> CustomerAllergies { get; set; }
-    = new List<CustomerAllergy>();
-
-    public ICollection<CustomerChronicCondition> CustomerChronicConditions { get; set; }
-        = new List<CustomerChronicCondition>();
-
 
 
 }
