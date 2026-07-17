2e8c90e53c838538070d10a0efde4f8593f9b375
Ziad Zaki <ziad.mahmoud151@gmail.com>
2026-07-17 20:35:21 +0300

    added medicine search controller

---
+++ SafePharma.API/Controllers/MedicineSearchController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SafePharma.BLL;
+using System.Security.Claims;
+
+namespace SafePharma.API.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class MedicineSearchController : ControllerBase
+    {
+        private readonly IMedicineSearchService _searchService;
+
+        public MedicineSearchController(IMedicineSearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(
+            [FromQuery] MedicineSearchRequestDto request)
+        {
+            var pharmacyIdClaim = User.FindFirstValue("PharmacyId");
+
+            if (!Guid.TryParse(pharmacyIdClaim, out var pharmacyId))
+                return Unauthorized();
+
+            var result = await _searchService.SearchAsync(pharmacyId, request);
+
+            return Ok(result);
+        }
+    }
+}
