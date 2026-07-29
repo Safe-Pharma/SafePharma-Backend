aee818a54eb3dd50a8da9d0a0de75ecc644bb890
Ziad Zaki <ziad.mahmoud151@gmail.com>
2026-07-30 00:21:26 +0300

    fix: enforce pharmacy-scoped access in PurchaseReceipt flow + add Authorize

--- SafePharma.BLL/Managers/PurchaseReceiptManager/IPurchaseReceiptManager.cs
+++ SafePharma.BLL/Managers/PurchaseReceiptManager/IPurchaseReceiptManager.cs
@@ -1,12 +1,11 @@
-﻿
-using SafePharma.Common;
+﻿using SafePharma.Common;
 
 namespace SafePharma.BLL
 {
     public interface IPurchaseReceiptManager
     {
-        Task<GeneralResult<IEnumerable<ReadPurchaseReceiptDto>>> GetAllPurchaseReceipts();
-        Task<GeneralResult<ReadPurchaseReceiptDto?>> CreatePurchaseReceipt(CreatePurchaseReceiptDto createDto, Guid userId, Guid purchaseOrderId);
-        Task<GeneralResult> UpdateReceiptItem(Guid id, UpdatePurchaseReceiptItemDto dto);
+        Task<GeneralResult<IEnumerable<ReadPurchaseReceiptDto>>> GetAllPurchaseReceipts(Guid pharmacyId);
+        Task<GeneralResult<ReadPurchaseReceiptDto?>> CreatePurchaseReceipt(CreatePurchaseReceiptDto createDto, Guid userId, Guid purchaseOrderId, Guid pharmacyId);
+        Task<GeneralResult> UpdateReceiptItem(Guid id, UpdatePurchaseReceiptItemDto dto, Guid pharmacyId);
     }
 }
